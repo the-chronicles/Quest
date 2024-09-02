cfcdf37e7ea140afbd813eb757bfb4d1fe9d2eff
Oluwaseun Akeredolu <oluseungideon@gmail.com>
2024-09-02 22:20:28 +0100

    features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
 
 function Features() {
   return (
-    <div>Features</div>
-  )
+    <section
+      id="features"
+      className="mx-4 flex h-full items-center justify-center p-5 m-5"
+    >
+      <div className="flex flex-col items-center justify-center font-[Inter]">
+        <div className="w-fit items-center justify-center rounded-full border-2  bg-[#FAFAFD] border-[#CFD3EF] px-4 py-2 text-[#747999]">
+          Quest's Features
+        </div>
+        <div className="text-center">
+          <h1 className="mb-4 mt-5 font-semibold text-[#060E23] md:text-5xl">
+            Your Trusted Partner in <br /> Financial Security
+          </h1>
+          <p className="mb-4 text-xs text-[#72758A] md:text-lg">
+            Comprehensive Security Solutions for the Financial Industry.
+          </p>
+        </div>
+        <div className="grid-col-1 grid gap-2 px-36 text-center font-[Inter] md:grid-cols-4">
+          <div className="rounded-md bg-[#f8f9fb] px-5 pt-5 md:col-span-2">
+            <h1 className="text-xl font-semibold text-[#060E23]">
+              Secure Data Management
+            </h1>
+            <p className="text-[#72758A]">
+              Industry-leading encryption and data protection solutions to
+              safeguard sensitive information.
+            </p>
+            <img src="/Pattern.png" alt="" className="flex items-baseline" />
+          </div>
+          <div className="rounded-md bg-[#f8f9fb] px-5 pt-5 md:col-span-2">
+            <h1 className="text-xl font-semibold text-[#060E23]">
+              Advanced Threat Detection
+            </h1>
+            <p className="text-[#72758A]">
+              Real-time monitoring and rapid response to emerging cyber threats.
+            </p>
+            <img src="/Figure.png" alt="" className="flex items-baseline" />
+          </div>
+          <div className="rounded-md bg-[#f8f9fb] md:col-span-1">
+            <h1>Customized Security Strategies</h1>
+          </div>
+          <div className="rounded-md bg-[#f8f9fb] md:col-span-3">
+            <h1>Quest Vault™</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Features
+export default Features;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,21 +2,22 @@ import React from "react";
 
 function Hero() {
   return (
-    <section className="flex rounded-b-2xl border-b-2 h-full mx-4 items-center justify-center bg-gradient-to-b from-white to-transparent">  {/* h-screen  */}
-      <div className=" text-center font-[Inter]">
-        <div className="bg-gradient-to-b from-[#060E23] to-[#060e2381] bg-clip-text text-3xl font-bold text-transparent">
-          <h1 className="mb-4 mt-5 md:text-5xl">
+    <section className="mx-4 flex h-full items-center justify-center rounded-b-2xl border-b-2 bg-gradient-to-b from-white to-transparent">
+      {/* h-screen  */}
+      <div className="text-center font-[Inter]">
+        <div className="text-3xl">
+          <h1 className="mb-4 mt-5 font-semibold text-[#060E23] md:text-5xl">
             Protecting Your <br />
             Financial Future, Today.
           </h1>
-          <p className="mb-4 text-xs md:text-lg">
+          <p className="mb-4 text-xs text-[#72758A] md:text-lg">
             Protecting Financial Institutions with Tailored, Cutting-Edge
             Security Solutions <br />
             that Safeguard Assets, Ensure Compliance, and Preserve Trust.
           </p>
         </div>
 
-        <div className="flex justify-center gap-4 mb-9">
+        <div className="mb-9 flex justify-center gap-4">
           <div>
             <a href="#">
               <button className="rounded-full border-2 border-[#1835F2] bg-gradient-to-tr from-[#1835F2] to-blue-500 px-4 py-2 text-white">
